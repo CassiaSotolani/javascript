facd92213457267af9ac0c2f9ad0964027ee5805
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-20 19:24:22 -0400

    Operações aritmeticas e tipos de dados

---
+++ js/oparacoes_aritmeticas.js
@@ -0,0 +1,57 @@
+/**
+ * Operações Aritméticas
+ *  - Adição (+)
+ *  - Subtração (-)
+ *  - Multiplicação (*)
+ *  - Divisão (/)
+ *  - Modulo (%)
+ *  - Incremento (++)
+ *  - Decremento (--)
+ *      obs: no incremento e decremento, diferença de ++a e a++
+ *           a++ - o incremento ocorre depois
+ *           ++a - o incremento ocorre antes
+ */
+
+// // Adição
+// const a = 10;
+// const b = 15;
+// const c = a + b;
+// console.log(c);
+
+// // Subtração
+// const a = 10;
+// const b = 15;
+// const c = a - b;
+// console.log(c);
+
+// // Multiplicação
+// const a = 10;
+// const b = 15;
+// const c = a * b;
+// console.log(c);
+
+// // Divisão
+// const a = 10;
+// const b = 15;
+// const c = a / b;
+// console.log(c);
+
+// // Modulo
+// const a = 10;
+// const b = 15;
+// const c = a % b;
+// console.log(c);
+
+// // Incremento
+// let a = 10;
+// a++;
+// console.log(a);
+
+// Decremento
+let a = 10;
+let b = --a
+// console.log(b);
+
+let a1 = 10;
+let b1 = a1--
+// console.log(b1);
---
+++ js/tipos_de_dados.js
@@ -0,0 +1,24 @@
+/**
+ * Tipos de dados
+ *  - Boolean
+ *  - Null
+ *  - Undefined
+ *  - Number
+ *  - String
+ */
+
+// String - Cadeias de caracteres
+// soma de strings é concatenação
+// soma de string e numero é concatenação
+const string = ' ';
+const string2 = '';
+const string3 = 'Teste da primeira string';
+const string4 = 'Teste';
+
+// Number
+const numero = 10;
+const numero2 = 10.5;
+
+// Undefined - Não definido - não sabe o que tem dentro
+// Null - é a certeza de que não tem nada dentro da variável
+// Boolean - verdadeiro ou falso - serve para fazer operações logicas
