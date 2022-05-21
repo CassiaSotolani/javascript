1fc6276216eac8b707600f4ac60582b69d962df6
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-21 17:34:23 -0400

    Aprendendo usar switch case

---
+++ js/switch.js
@@ -0,0 +1,36 @@
+var nota1 = 10.0;
+var nota2 = 3.0;
+
+var media = (nota1 + nota2) / 2;
+
+var conceito = '';
+
+if (media >= 8) {
+    conceito = 'Ótimo!';
+} else if (media >= 6.5) {
+    conceito = 'Bom!';
+} else {
+    conceito = 'Regular!';
+}
+
+switch (conceito) {
+    case 'Ótimo!': {
+        console.log('Parabéns! Você é um ótimo aluno.');
+        break;
+    }
+
+    case 'Bom!': {
+        console.log('Você esta quase perfeito.');
+        break;
+    }
+
+    case 'Regular!': {
+        console.log('Estude mais um pouco.');
+        break;
+    }
+
+    default: {
+        console.log('Houve algum erro.');
+        break;
+    }
+}
