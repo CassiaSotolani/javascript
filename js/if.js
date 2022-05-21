2cf680173d872c9aac7c525c34894ce880c776d0
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-20 21:32:14 -0400

    Comentando codigo que não será mais usado

--- js/if.js
+++ js/if.js
@@ -1,13 +1,13 @@
-var nome = prompt('Qual seu nome?');
-var idade = parseInt(prompt('Qual sua idade?'));
+// var nome = prompt('Qual seu nome?');
+// var idade = parseInt(prompt('Qual sua idade?'));
 
-if(idade >= 35) {
-    document.write(`Pode entrar, ${nome}!<br>`);
-    var pedido = prompt('Qual seu pedido?');
-    document.write(`Seu pediu foi ${pedido}`);
-} else if (idade >= 18) {
-    document.write(`Pode entrar, ${nome}!<br>`);
-    document.write(`Mostre sua identidade!`);
-} else {
-    document.write(`Você tem ${idade} anos, ${nome} e por isso não pode entrar! Volte futuramente!`);
-}
+// if(idade >= 35) {
+//     document.write(`Pode entrar, ${nome}!<br>`);
+//     var pedido = prompt('Qual seu pedido?');
+//     document.write(`Seu pediu foi ${pedido}`);
+// } else if (idade >= 18) {
+//     document.write(`Pode entrar, ${nome}!<br>`);
+//     document.write(`Mostre sua identidade!`);
+// } else {
+//     document.write(`Você tem ${idade} anos, ${nome} e por isso não pode entrar! Volte futuramente!`);
+// }
