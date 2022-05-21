62f45ec2891c22793daefa81a4f5a2ed27a89722
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-20 20:43:59 -0400

    Operadores de comparação e operadores lógicos

---
+++ js/operadores_comparativos.js
@@ -0,0 +1,23 @@
+/**
+ * Operadores de Comparação
+ *  == igual a
+ *  === além do valor o tipo do dado também é igual
+ *  != não é igual
+ *  !== além do valor o tipo do dado também não é igual
+ *  > maior que
+ *  < menor que
+ *  >= maior ou igual a
+ *  <= menor ou igual
+ */
+
+
+/**
+ * Operadores Lógicos
+ *  && = e
+ *  || = ou
+ *  Negação = !
+ */
+
+// usando negação
+console.log(!true);
+console.log(!false);
