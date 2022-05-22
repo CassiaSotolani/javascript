3632786c584160ffadb2bdbdac4cc6f09358679c
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-21 21:13:20 -0400

    Trabalhando com for in, for of e for

---
+++ js/array.js
@@ -0,0 +1,16 @@
+// var alunos = ['Cassia', 'Luana', 'Sônia', 'Samanta', 'Susu', 'Bia'];
+
+// // for normal
+// for (var i = 0; i < alunos.length; i++) {
+//     console.log(alunos[i]);
+// }
+
+// // for in pega o índice
+// for (var i in alunos) {
+//     console.log(alunos[i]);
+// }
+
+// // for of pega o valor mesmo. o elemento daquele índice
+// for (var aluno of alunos) {
+//     console.log(aluno);
+// }
