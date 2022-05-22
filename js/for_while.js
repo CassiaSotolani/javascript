8242da5894b7a584b336af0be5a967e2064ea820
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-21 20:58:32 -0400

    Trabalhando com for e while

---
+++ js/for_while.js
@@ -0,0 +1,10 @@
+// var numero1 = 10;
+// for (var i = 1; i <= numero1; i++) {
+//     console.log(`Executando for pela ${i} vez!`);
+// }
+
+// var contador = 1;
+// while (contador <= numero1) {
+//     console.log(`Executando while pela ${contador} vez!`);
+//     contador++;
+// }
