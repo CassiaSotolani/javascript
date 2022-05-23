69a700f9e3e1f0cf66eb6429009ffc62440ce322
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-23 19:04:09 -0400

    Aprendendo outras formas de criar objeto em js

--- js/objetos.js
+++ js/objetos.js
@@ -1,3 +1,4 @@
+// objeto é a relação de uma chave e um valor (key => value)
 var aluno = {
     nome: 'Cassia',
     sobrenome: 'Sotolani',
@@ -9,3 +10,20 @@ var aluno = {
 console.log(aluno['notas'][0]);
 console.log(aluno.notas[1]);
 console.log(aluno.nome);
+
+// formas de adicionar novas propriedades em um objeto
+aluno.matricula = 123456;
+aluno['materia'] = 'Matemática';
+
+var novaPropriedade = 'materia2';
+aluno[novaPropriedade] = 'Português';
+
+console.log(aluno);
+
+// outra forma de criar um objeto
+var aluno2 = new Object();
+
+aluno2.nome = 'Luana';
+aluno2.sobrenome = 'Vincenzi';
+
+console.log(aluno2.nome);
