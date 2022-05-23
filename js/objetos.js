fd91451ba71f9fca435101f0fcc2e46a01aa14a5
Cassia Sotolani <ca.sotolani@gmail.com>
2022-05-23 18:54:18 -0400

    Aprendendo sobre objetos

---
+++ js/objetos.js
@@ -0,0 +1,11 @@
+var aluno = {
+    nome: 'Cassia',
+    sobrenome: 'Sotolani',
+    idade: 30,
+    ano: 9,
+    notas: [10, 8.5]
+}
+
+console.log(aluno['notas'][0]);
+console.log(aluno.notas[1]);
+console.log(aluno.nome);
